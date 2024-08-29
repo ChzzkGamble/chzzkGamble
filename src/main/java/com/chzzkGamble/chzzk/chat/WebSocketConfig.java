81a5426c1936e0694de35e07d633841a66f52111
tsulocalize <tsulocalizing@gmail.com>
2024-08-30 02:31:34 +0900

    feat: 메시지 버퍼 크기 증가

---
+++ src/main/java/com/chzzkGamble/chzzk/chat/WebSocketConfig.java
@@ -0,0 +1,13 @@
+package com.chzzkGamble.chzzk.chat;
+
+import jakarta.websocket.ContainerProvider;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSocketConfig {
+
+    public WebSocketConfig() {
+        ContainerProvider.getWebSocketContainer()
+                .setDefaultMaxBinaryMessageBufferSize(10 * 512 * 1024); // default : 512 * 1024
+    }
+}
