3b1d2ab2046452ded1a1aee6e3c92df383ce773b
tsulocalize <tsulocalizing@gmail.com>
2024-09-01 15:40:28 +0900

    fix: 웹소켓 버퍼 사이즈 수정

--- src/main/java/com/chzzkGamble/config/WebSocketConfig.java
+++ src/main/java/com/chzzkGamble/config/WebSocketConfig.java
@@ -1,13 +1,15 @@
 package com.chzzkGamble.config;
 
 import jakarta.websocket.ContainerProvider;
+import jakarta.websocket.WebSocketContainer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebSocketConfig {
 
     public WebSocketConfig() {
-        ContainerProvider.getWebSocketContainer()
-                .setDefaultMaxBinaryMessageBufferSize(10 * 512 * 1024); // default : 512 * 1024
+        WebSocketContainer webSocketContainer = ContainerProvider.getWebSocketContainer();
+        webSocketContainer.setDefaultMaxBinaryMessageBufferSize(100 * 8 * 1024); // default : 8192 Bytes
+        webSocketContainer.setDefaultMaxTextMessageBufferSize(100 * 8 * 1024); // default : 8192 Bytes
     }
 }
