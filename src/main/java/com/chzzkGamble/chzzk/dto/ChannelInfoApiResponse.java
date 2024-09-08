9a08eeb94e3407c1fc4ff1cab95b5ac9b9c76e69
tsulocalize <tsulocalizing@gmail.com>
2024-09-08 19:23:20 +0900

    feat: 룰렛 생성시 채널 정보 응답하는 기능 추가

--- src/main/java/com/chzzkGamble/chzzk/dto/ChannelInfoApiResponse.java
+++ src/main/java/com/chzzkGamble/chzzk/dto/ChannelInfoApiResponse.java
@@ -8,4 +8,5 @@ import lombok.ToString;
 public class ChannelInfoApiResponse {
 
     private String channelName;
+    private String channelImageUrl;
 }
--- src/main/java/com/chzzkGamble/gamble/roulette/controller/RouletteController.java
+++ src/main/java/com/chzzkGamble/gamble/roulette/controller/RouletteController.java
@@ -2,6 +2,7 @@ package com.chzzkGamble.gamble.roulette.controller;
 
 import com.chzzkGamble.chzzk.api.ChzzkApiService;
 import com.chzzkGamble.chzzk.chat.ChzzkChatService;
+import com.chzzkGamble.chzzk.dto.ChannelInfoApiResponse;
 import com.chzzkGamble.exception.ChzzkException;
 import com.chzzkGamble.exception.ChzzkExceptionCode;
 import com.chzzkGamble.gamble.roulette.domain.Roulette;
@@ -12,6 +13,7 @@ import com.chzzkGamble.gamble.roulette.service.RouletteService;
 import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -33,9 +35,11 @@ public class RouletteController {
     private final ChzzkChatService chzzkChatService;
 
     @PostMapping("/create")
-    public ResponseEntity<Void> createRoulette(@RequestBody RouletteCreateRequest request) {
+    public ResponseEntity<ChannelInfoApiResponse> createRoulette(@RequestBody RouletteCreateRequest request) {
         String channelId = request.getChannelId();
-        String channelName = chzzkApiService.getChannelInfo(channelId).getChannelName();
+
+        ChannelInfoApiResponse channelInfo = chzzkApiService.getChannelInfo(channelId);
+        String channelName = channelInfo.getChannelName();
         if (channelName.equals(INVALID_CHANNEL_NAME)) {
             throw new ChzzkException(ChzzkExceptionCode.CHANNEL_ID_INVALID, "channelId : " + channelId);
         }
@@ -49,7 +53,9 @@ public class RouletteController {
                 .maxAge(2 * 60 * 60L) // 2 hours
                 .build();
 
-        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).build();
+        return ResponseEntity.status(HttpStatus.OK)
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(channelInfo);
     }
 
     @PostMapping("/start")
