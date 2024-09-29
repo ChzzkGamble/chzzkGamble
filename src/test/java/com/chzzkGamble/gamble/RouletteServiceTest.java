23f485696889d57a24ef45d04a977908296e2f50
tsulocalize <tsulocalizing@gmail.com>
2024-09-29 20:10:14 +0900

    test: 깨지는 테스트 수정
    - 이전 테스트 영향 받지 않도록 수정
    - 투표 시작하도록 수정

--- src/test/java/com/chzzkGamble/gamble/RouletteServiceTest.java
+++ src/test/java/com/chzzkGamble/gamble/RouletteServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
@@ -18,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class RouletteServiceTest {
 
     private static final String CHANNEL_ID = "ch_id";
@@ -108,6 +110,7 @@ public class RouletteServiceTest {
         rouletteElementRepository.save(new RouletteElement("요소", 0 ,roulette));
 
         // when & then
+        rouletteService.startVote(roulette.getId());
         assertThatThrownBy(() -> rouletteService.vote(CHANNEL_NAME, "<요소>", -1_000))
                 .isInstanceOf(IllegalArgumentException.class);
     }
