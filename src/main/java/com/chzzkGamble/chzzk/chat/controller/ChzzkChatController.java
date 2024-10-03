d703aacad0fe319c0e3726a155f988d2238d47de
tsulocalize <tsulocalizing@gmail.com>
2024-10-03 17:52:46 +0900

    refactor: 채널 연결 상태 api 재생성

--- src/main/java/com/chzzkGamble/chzzk/chat/controller/ChzzkChatController.java
+++ src/main/java/com/chzzkGamble/chzzk/chat/controller/ChzzkChatController.java
@@ -7,8 +7,11 @@ import com.chzzkGamble.chzzk.dto.ChannelInfoApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,5 +32,13 @@ public class ChzzkChatController {
         chzzkChatService.connectChatRoom(channelName);
 
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/check")
+    public ResponseEntity<Void> checkConnection(@RequestParam String channelName) {
+        if (chzzkChatService.isConnected(channelName)) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
