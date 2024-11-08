91a7af55ac5ec18b43d7230a5ce4594f261e8989
jimi567 <28584160+jimi567@users.noreply.github.com>
2024-11-08 20:50:11 +0900

    refactor: 상수명 변경

--- src/main/java/com/chzzkGamble/chzzk/chat/service/ChzzkWebSocketClient.java
+++ src/main/java/com/chzzkGamble/chzzk/chat/service/ChzzkWebSocketClient.java
@@ -1,13 +1,13 @@
 package com.chzzkGamble.chzzk.chat.service;
 
+import com.chzzkGamble.chzzk.api.ChzzkApiService;
+import com.chzzkGamble.exception.ChzzkException;
+import com.chzzkGamble.exception.ChzzkExceptionCode;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import com.chzzkGamble.chzzk.api.ChzzkApiService;
-import com.chzzkGamble.exception.ChzzkException;
-import com.chzzkGamble.exception.ChzzkExceptionCode;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketSession;
@@ -17,7 +17,7 @@ import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 public class ChzzkWebSocketClient {
 
     private static final String CHZZK_CHAT_SERVER = "wss://kr-ss";
-    private static final String ChZZK_CHAT_SERVER2 = ".chat.naver.com/chat";
+    private static final String CHZZK_CHAT_SERVER2 = ".chat.naver.com/chat";
 
     private final WebSocketClient client = new StandardWebSocketClient();
     private final WebSocketHandler handler;
@@ -30,9 +30,11 @@ public class ChzzkWebSocketClient {
     }
 
     public void connect() {
+        //각 앤드포인트 마다 서버 5개 1~5
         int serverId = ThreadLocalRandom.current().nextInt(1, 6);
         try {
-            session = client.execute(handler, CHZZK_CHAT_SERVER + serverId + ChZZK_CHAT_SERVER2).get(30, TimeUnit.SECONDS);
+            session = client.execute(handler, CHZZK_CHAT_SERVER + serverId + CHZZK_CHAT_SERVER2)
+                    .get(30, TimeUnit.SECONDS);
             session.setTextMessageSizeLimit(64 * 1024); // 64 KB
         } catch (TimeoutException | ExecutionException | InterruptedException e) {
             throw new ChzzkException(ChzzkExceptionCode.CHAT_CONNECTION_ERROR, e.getMessage());
