9bacd8fe608139ae5ca5f9deed83eca81e72f45b
김기범(Kibum kim) <jimi567@kookmin.ac.kr>
2024-11-09 12:04:15 +0900

    룰렛 리팩토링 (#4)
    * refactor: Roulette dto record로 변경
    
    * refactor: 룰렛이 존재하는지 판단하는 query 변경
    
    * refactor: 상수명 변경

--- src/main/java/com/chzzkGamble/gamble/roulette/domain/RouletteElement.java
+++ src/main/java/com/chzzkGamble/gamble/roulette/domain/RouletteElement.java
@@ -6,14 +6,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import java.util.Objects;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@EqualsAndHashCode(of = "id")
 @ToString
 public class RouletteElement {
 
@@ -35,21 +36,9 @@ public class RouletteElement {
     }
 
     public void increaseCount(int amount) {
-        if (amount <= 0 || count + amount < 0) throw new IllegalArgumentException("잘못된 상승 수치입니다. amount : " + amount);
+        if (amount <= 0 || count + amount < 0) {
+            throw new IllegalArgumentException("잘못된 상승 수치입니다. amount : " + amount);
+        }
         count += amount;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        RouletteElement that = (RouletteElement) o;
-        return Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
 }
-
--- src/main/java/com/chzzkGamble/gamble/roulette/repository/RouletteRepository.java
+++ src/main/java/com/chzzkGamble/gamble/roulette/repository/RouletteRepository.java
@@ -1,10 +1,10 @@
 package com.chzzkGamble.gamble.roulette.repository;
 
+import com.chzzkGamble.gamble.roulette.domain.Roulette;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import com.chzzkGamble.gamble.roulette.domain.Roulette;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RouletteRepository extends JpaRepository<Roulette, UUID> {
@@ -14,4 +14,6 @@ public interface RouletteRepository extends JpaRepository<Roulette, UUID> {
     Optional<Roulette> findByIdAndCreatedAtAfter(UUID id, LocalDateTime dateTime);
 
     List<Roulette> findByChannelNameAndVotingIsTrue(String channelName);
+
+    boolean existsByChannelNameAndVotingIsTrue(String channelName);
 }
