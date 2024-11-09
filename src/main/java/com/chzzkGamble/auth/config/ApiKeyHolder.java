1594fb11d425007e854b735cc8541011019e5c5f
unifolio0 <boho82ha@gmail.com>
2024-11-09 13:50:48 +0900

    refactor: 불필요한 어노테이션 제거

--- src/main/java/com/chzzkGamble/auth/config/ApiKeyHolder.java
+++ src/main/java/com/chzzkGamble/auth/config/ApiKeyHolder.java
@@ -2,9 +2,7 @@ package com.chzzkGamble.auth.config;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class ApiKeyHolder {
 
     @Getter
