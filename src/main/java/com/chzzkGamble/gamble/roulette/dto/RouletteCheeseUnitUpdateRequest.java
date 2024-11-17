5e6c7f154239177ebca94c40d9fdacbced760803
jimi567 <28584160+jimi567@users.noreply.github.com>
2024-11-17 09:01:15 +0900

    feat: dto 양수 검증 추가

--- src/main/java/com/chzzkGamble/gamble/roulette/dto/RouletteCheeseUnitUpdateRequest.java
+++ src/main/java/com/chzzkGamble/gamble/roulette/dto/RouletteCheeseUnitUpdateRequest.java
@@ -1,9 +1,11 @@
 package com.chzzkGamble.gamble.roulette.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 
 public record RouletteCheeseUnitUpdateRequest(
-        @NotBlank(message = "룰렛 단위를 입력해주세요.")
+        @Positive(message = "룰렛 치즈 단위는 0이상입니다.")
+        @NotBlank(message = "룰렛 치즈 단위를 입력해주세요.")
         Integer rouletteUnit
 ) {
 }
