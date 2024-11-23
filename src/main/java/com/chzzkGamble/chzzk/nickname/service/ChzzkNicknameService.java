ba7dcfaf4d34face4dc62cc8393248cac63fe805
tsulocalize <tsulocalizing@gmail.com>
2024-11-23 18:20:02 +0900

    feat: 닉네임 서비스 기능 구현 및 테스트 추가

---
+++ src/main/java/com/chzzkGamble/chzzk/nickname/service/ChzzkNicknameService.java
@@ -0,0 +1,19 @@
+package com.chzzkGamble.chzzk.nickname.service;
+
+import com.chzzkGamble.chzzk.nickname.domain.ChannelName;
+import org.springframework.stereotype.Service;
+import java.util.Map;
+
+@Service
+public class ChzzkNicknameService {
+
+    private final Map<String, ChannelName> nicknames = ChzzkNicknameReader.readNicknameData();
+
+    public String getChannelNameFromNickname(String nickname) {
+        System.out.println("nicknames = " + nicknames);
+        if (nicknames.containsKey(nickname)) {
+            return nicknames.get(nickname).value();
+        }
+        return nickname;
+    }
+}
