2fb8b5a7b6c507d135ee70d92322a42d7e3622c4
tsulocalize <tsulocalizing@gmail.com>
2024-12-04 22:48:45 +0900

    fix: 컬럼명 오류 수정

--- src/test/resources/roulette.sql
+++ src/test/resources/roulette.sql
@@ -1,4 +1,4 @@
-INSERT INTO roulette(channel_name, voting, created_at, cheese_unit)
+INSERT INTO roulette(channel_name, voting, created_at, roulette_unit)
 VALUES ('ch_name', true, '2024-08-30T00:00:00', 1000),
        ('ch_name', true, '2024-08-30T02:00:00', 1000),
        ('ch_name', true, '2024-08-30T04:00:00', 1000),
