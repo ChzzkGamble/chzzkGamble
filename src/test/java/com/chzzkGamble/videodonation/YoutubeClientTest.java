032f72275bf6b66bb9dc5622c81ac0dc1b44259b
tsulocalize <tsulocalizing@gmail.com>
2024-12-11 15:08:51 +0900

    test: youtube api 테스트 케이스 추가

--- src/test/java/com/chzzkGamble/videodonation/YoutubeClientTest.java
+++ src/test/java/com/chzzkGamble/videodonation/YoutubeClientTest.java
@@ -4,10 +4,13 @@ import com.chzzkGamble.videodonation.youtube.YoutubeClient;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import java.util.stream.Stream;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -17,19 +20,26 @@ class YoutubeClientTest {
     private YoutubeClient client;
 
     @Disabled("실제 유튜브 서버로 API를 쏘는거라 필요할 때만 키는걸로.. "
-            + "진짜 api 요청이 잘가고, 데이터 잘 뽑아오는지 보고싶을 때 쓰면 될 것 같습니다."
-            + "테스트할 때, 프로덕션 코드 youtube 관련 설정 test 쪽에 추가하고 하세요"
-            + "실수로라도 커밋하시면 안됩니다. API Key 있어요.")
-    @Test
+            + "진짜 api 요청이 잘가고, 데이터 잘 뽑아오는지 보고싶을 때 쓰면 될 것 같습니다.")
+    @ParameterizedTest
+    @MethodSource("titleAndVideoId")
     @DisplayName("제목을 통해 영상 id을 추출한다.")
-    void getVideoIdByTitle() {
-        //given
-        String title = "[10분 테코톡] 우주의 낙관적인 락, 비관적인 락";
-
+    void getVideoIdByTitle(String title, String videoId) {
         //when
         String actual = client.getVideoIdByTitle(title);
 
         //then
-        Assertions.assertThat(actual).isEqualTo("era8W7q3CeQ");
+        Assertions.assertThat(actual).isEqualTo(videoId);
+    }
+
+    private static Stream<Arguments> titleAndVideoId() {
+        return Stream.of(
+                Arguments.of("[10분 테코톡] 우주의 낙관적인 락, 비관적인 락", "era8W7q3CeQ"),
+                Arguments.of("[1080P 60FPS] 【まいたけダンス】時間が巻いたときや巻きたいときにご利用ください【儒烏風亭らでん】maitake", "82CBG8QeWnY"),
+                Arguments.of("aespa 에스파 'Whiplash' MV", "jWQx2f-CErU"),
+                Arguments.of("듣고또듣고 Live-through\uD83C\uDFA7 이문세(Moon-Sae Lee) 옛사랑,깊은밤을날아서, 가로수그늘아래서면,소녀,사랑이 지나가면,붉은노을,광화문연가,휘파람,가을이 오면..", "nic2pkI-eHQ"),
+                Arguments.of("Kid perfectly recreates Bully Maguire dance #spiderman #tobeymaguire #shorts", "t6CKTjM9dGg"),
+                Arguments.of("Deadpool Dance Bye Bye Bye / Step by Step / NSYNC #deadpooldance #viraldancevideo #deadpool #nsync", "ByU-UGtg3Jc")
+        );
     }
 }
