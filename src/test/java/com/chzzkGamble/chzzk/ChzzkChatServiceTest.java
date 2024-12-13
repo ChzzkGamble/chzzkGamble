650306d17ce95a0c6e67104518a8276e9a89f3e1
tsulocalize <tsulocalizing@gmail.com>
2024-12-13 15:48:46 +0900

    test: 연결 제한 시간 변경에 따른 테스트 수정

--- src/test/java/com/chzzkGamble/chzzk/ChzzkChatServiceTest.java
+++ src/test/java/com/chzzkGamble/chzzk/ChzzkChatServiceTest.java
@@ -30,7 +30,7 @@ public class ChzzkChatServiceTest {
     private static final String CHANNEL_NAME = "따효니";
     private static final WebSocketMessage<?> message = new TextMessage("{\"svcid\":\"game\",\"ver\":\"1\",\"bdy\":[{\"svcid\":\"game\",\"cid\":\"N1Q-4g\",\"mbrCnt\":5548,\"uid\":\"anonymous\",\"profile\":null,\"msg\":\"<차렷자세>에서 손이 따봉으로 바뀔수는 있어도 크게 벗어나는 자세는 인싸다....\",\"msgTypeCode\":10,\"msgStatusType\":\"NORMAL\",\"extras\":\"{\\\"isAnonymous\\\":true,\\\"payType\\\":\\\"CURRENCY\\\",\\\"payAmount\\\":1000,\\\"donationId\\\":\\\"2wQtTN5iMhyeiRXLADF4GyUQ7hfmZ\\\",\\\"donationType\\\":\\\"CHAT\\\",\\\"weeklyRankList\\\":[]}\",\"ctime\":1727940364316,\"utime\":1727940364316,\"msgTid\":null,\"msgTime\":1727940364312}],\"cmd\":93102,\"tid\":null,\"cid\":\"N1Q-4g\"}");
     private static final Clock PAST_CLOCK = Clock.fixed(Instant.parse("2024-08-24T11:00:00Z"), ZoneId.of("Asia/Seoul"));
-    private static final Clock FUTURE_CLOCK = Clock.fixed(Instant.parse("2024-08-24T11:20:00Z"), ZoneId.of("Asia/Seoul"));
+    private static final Clock FUTURE_CLOCK = Clock.fixed(Instant.parse("2024-08-24T12:20:00Z"), ZoneId.of("Asia/Seoul"));
 
     @Autowired
     ApplicationEventPublisher eventPublisher;
