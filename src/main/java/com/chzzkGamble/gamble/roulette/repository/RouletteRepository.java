4c7438cd99cba69f3966627de80f03d894b8a5d3
jimi567 <28584160+jimi567@users.noreply.github.com>
2024-12-21 20:26:56 +0900

    feat: DB 쓰기락 부여

--- src/main/java/com/chzzkGamble/gamble/roulette/repository/RouletteRepository.java
+++ src/main/java/com/chzzkGamble/gamble/roulette/repository/RouletteRepository.java
@@ -1,17 +1,27 @@
 package com.chzzkGamble.gamble.roulette.repository;
 
 import com.chzzkGamble.gamble.roulette.domain.Roulette;
+import jakarta.persistence.LockModeType;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RouletteRepository extends JpaRepository<Roulette, UUID> {
 
     List<Roulette> findByChannelNameAndCreatedAtIsAfter(String channelName, LocalDateTime dateTime);
 
     Optional<Roulette> findByIdAndCreatedAtAfter(UUID id, LocalDateTime dateTime);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT r "
+            + "FROM Roulette AS r "
+            + "WHERE r.id = :id AND r.createdAt > :dateTime ")
+    Optional<Roulette> findByIdAndCreatedAtAfterWithLock(@Param("id") UUID id, @Param("dateTime")LocalDateTime dateTime);
 
     List<Roulette> findByChannelNameAndVotingIsTrue(String channelName);
 
